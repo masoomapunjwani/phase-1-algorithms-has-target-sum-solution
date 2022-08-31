53408df01ed553b685c9f6ed41fb6497a9914ff0
masoomahasnain <masooma.punjwani@student.moringaschool.com>
2022-08-31 19:35:07 +0300

    completed asignment

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const obj = {}
+  //iterate over each array
+  for(const number of array){
+    const complement = target - number;
+    if(obj[complement]) return true
+    obj[number] = true;
+  }
+  return false;
 }
 
+
 /* 
+
   Write the Big O time complexity of your function here
+  Time complexity: 0(n)
+  Space complexity: 0(n)
 */
 
 /* 
   Add your pseudocode here
 */
-
+  //make a function that takes two parameters
+    //make an emty object
+    //iterate over each number of array
+      //make a variable called complement that minus target and number and store the value to complement variable
+      //check if object has the value of complement if it has , returns true
+  //otherwise returns false 
 /*
   Add written explanation of your solution here
 */
